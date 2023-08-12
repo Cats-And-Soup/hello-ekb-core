ea4f979816b5b4c71ecf56ce77ad1606c721fb6e
Lev <vinnipyx38@gmail.com>
2023-08-12 15:34:06 +0500

    Init commit

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+
+from sqlalchemy import Boolean, Column, Integer, String, Enum
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+from app.shared.types import Roles
+
+if TYPE_CHECKING:
+    from .item import Item  # noqa: F401
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(Enum(Roles), default=Roles.user)
